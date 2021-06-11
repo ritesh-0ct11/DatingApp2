31edcef2b1ac508e5cc610f868b731314a9d63a8
Ritesh Gupta <ritesh.gupta3@concentrix.com>
2021-06-11 21:34:25 +0530

    End of section 10

---
+++ client/src/_services/busy.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BusyService {
+  bustRequestCount = 0;
+
+
+  constructor(private spinnerService: NgxSpinnerService) { }
+
+  busy(){
+    this.bustRequestCount++;
+    this.spinnerService.show(undefined, {
+      type: 'line-scale-party',
+      bdColor: 'rgba(255,255,255,0)',
+      color: '#333333'
+    });
+  }
+
+  idle(){
+    this.bustRequestCount--;
+    if(this.bustRequestCount <= 0){
+      this.bustRequestCount = 0;
+      this.spinnerService.hide();
+    }
+  }
+}
